33d9b841014055566afc913a03675570276e3556
downtowndebbie <karenlyuen@gmail.com>
2013-10-01 21:18:42 -0400

    committing Users exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Directory::Application.config.secret_key_base = '10cbde95ee4bb2f9067715729f1a714ab0b3223b0d941087e63f82982efaa12c52f4fa72943f4485f796c1a15d47b004a1a886d37e19d9c710492a4e2398ee0a'
